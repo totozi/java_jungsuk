5dce5a6e72748367dc4ea9db78925aaa24fa0aaf
skgydnjs@gmail.com <skgydnjs@gmail.com>
2023-06-12 00:12:31 +0900

    -all

---
+++ src/main/java/jungsuk/ch2/Ex2_1.java
@@ -0,0 +1,18 @@
+package jungsuk.ch2;
+
+public class Ex2_1 {
+    public static void main(String[] args) {
+
+        System.out.println( "1" + "2");
+        System.out.println( true + ""); // String
+        System.out.println( 'A' + 'B'); // char + char는 int형 정수값
+        System.out.println( '1' + 2);
+//        System.out.println( true + null); // 에러
+
+        // 문자열과 덧셈연산 -> 결과는 문자열
+        // but, 문자와 문자 덧셈은 int형 정수값 반환
+
+
+    }
+
+}
